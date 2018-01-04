d1a7e3f7c907a373a3c4209766c1ecd274d07666
Edward Kollar <kollare@mail.gvsu.edu>
2018-01-04 08:17:53 -0500

    Initial upload - "HelloWorldTest"
    The initial upload for: "HelloWorldTest".

---
+++ HelloWorldTest/HelloWorldTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Visual Studio Console Works...");
+            Console.ReadLine();
+        }
+    }
+}
